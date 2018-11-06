9632f1c64e13ae379c42a7d77beeecf445922eb6
Jamin Brako <jamin.brako@gmail.com>
2018-11-06 11:00:23 -0500

    finished hello code

---
+++ helloworld.js
@@ -0,0 +1,16 @@
+function sayC() {
+  console.log (Hello, console)
+}
+ function sayP() {
+   alert(Hello, Pop-Up)
+ }
+
+ function sayBrowser() {
+   Var p = document.getElementById(“Browser”);
+ 	p.innerHTML = “Hello, World!”;
+
+ }
+
+function sayStranger() {
+
+}
